05f0dacc6496f478eaa04fb9549cdc1691476c61
Dougal J. Sutherland <dougal@gmail.com>
2017-06-05 16:23:42 +0100

    update mathjax code

--- js/init.js
+++ js/init.js
@@ -1,26 +1,44 @@
+// figure out if offline mathjax is available, then initialize
+math_config = {
+    config: 'TeX-AMS_HTML-full'
+};
+mathjax_offline_path = 'node_modules/mathjax/MathJax.js';
+
+var client = new XMLHttpRequest();
+client.onreadystatechange = function() {
+    if (this.readyState == 4) {
+        if (this.status == 200) {
+            math_config.mathjax = mathjax_offline_path;
+        }  // otherwise use default from CDN
+        console.log("Mathjax status: " + this.status + "; initializing");
+        init();
+    }
+}
+console.log("Checking for offline mathjax...")
+client.open("HEAD", mathjax_offline_path);
+client.send();
 
 // More info: https://github.com/hakimel/reveal.js#configuration
-Reveal.initialize({
-    history: true,
-    progress: false,
-    controls: false,
+function init() {
+    Reveal.initialize({
+        history: true,
+        progress: true,
+        controls: false,
 
-    math: {
-        mathjax: 'node_modules/mathjax/MathJax.js',
-        config: 'TeX-AMS_HTML-full'
-    },
+        math: math_config,
 
-    keyboard: {
-        40: 'next',
-        38: 'prev'
-    },
+        keyboard: {
+            40: 'next',
+            38: 'prev'
+        },
 
-    // More info https://github.com/hakimel/reveal.js#dependencies
-    dependencies: [
-        { src: 'node_modules/reveal.js/plugin/markdown/marked.js' },
-        { src: 'node_modules/reveal.js/plugin/markdown/markdown.js' },
-        { src: 'node_modules/reveal.js/plugin/notes/notes.js', async: true },
-        { src: 'node_modules/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
-        { src: 'js/mymath.js', async: true }
-    ]
-});
+        // More info https://github.com/hakimel/reveal.js#dependencies
+        dependencies: [
+            { src: 'node_modules/reveal.js/plugin/markdown/marked.js' },
+            { src: 'node_modules/reveal.js/plugin/markdown/markdown.js' },
+            { src: 'node_modules/reveal.js/plugin/notes/notes.js', async: true },
+            { src: 'node_modules/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+            { src: 'js/mymath.js', async: true }
+        ]
+    });
+}
