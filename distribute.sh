f8a193da4e9f61557cae3d8fe095c99becf35220
Dougal J. Sutherland <dougal@gmail.com>
2017-06-13 14:29:34 +0100

    distribute.sh: include notes plugin page

--- distribute.sh
+++ distribute.sh
@@ -8,6 +8,7 @@ deps=$(git grep "[$qq]node_modules/[^$qq]*[$qq]" \
       | grep -v 'distribute.sh:' \
       | grep -o "node_modules/[^$qq]*" \
       | grep -v 'node_modules/mathjax/')
+deps="$deps node_modules/reveal.js/plugin/notes/notes.html"
 
 node_modules/.bin/grunt default
 tar czf talk.tar.gz index.html css js img $deps
