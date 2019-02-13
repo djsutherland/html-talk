cd71033f6cfcba8db22b1fbef5f11222e62979ac
Dougal J. Sutherland <dougal@gmail.com>
2019-02-13 22:37:45 +0000

    update video-autoplay.js to handle hiding fragments

--- js/video-autoplay.js
+++ js/video-autoplay.js
@@ -1,15 +1,37 @@
 (function () {
-  Reveal.addEventListener('fragmentshown', function (event) {
-    if (event.fragment.nodeName == 'VIDEO' &&
-        event.fragment.classList.contains('fragment-autoplay')) {
-      event.fragment.play();
+  function start(video) {
+    // TODO: ideally would use reveal handlers for data loading
+    //       but they're not in the API...
+    video.play();
+  }
+  function stop(video) {
+    video.pause();
+    video.currentTime = 0;
+  }
+
+  Reveal.addEventListener('fragmentshown', function(event) {
+    for (var elt of event.fragments) {
+      if (elt.nodeName == 'VIDEO' && elt.classList.contains('fragment-autoplay')) {
+        start(elt);
+      } else if (elt.classList.contains('fragment-autostop')) {
+        for (var video of elt.querySelectorAll('video.fragment-autoplay')) {
+          stop(video);
+        }
+      }
     }
   });
-  Reveal.addEventListener('fragmenthidden', function (event) {
-    if (event.fragment.nodeName == 'VIDEO' &&
-        event.fragment.classList.contains('fragment-autoplay')) {
-      event.fragment.pause();
-      event.fragment.currentTime = 0;
+
+  Reveal.addEventListener('fragmenthidden', function(event) {
+    for (var elt of event.fragments) {
+      if (elt.nodeName == 'VIDEO' && elt.classList.contains('fragment-autoplay')) {
+        stop(elt);
+      } else if (elt.classList.contains('fragment-autostop')) {
+        for (var video of elt.querySelectorAll('video.fragment-autoplay')) {
+          start(video);
+        }
+      }
     }
   });
+
+  // Reveal's stopEmbeddedContent() handles when we slide away for us
 })();
